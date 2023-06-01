b86c96b9f07ddea0bfcf959e533b8d0867a80063
Lisbon <lisbon@dublin.com>
2023-06-01 11:01:52 +0300

    Table- import plus string

---
+++ extra/table-2.js
@@ -0,0 +1,13 @@
+import daugiklis from "./multiplier";
+import {n1, n2, n3, n4, n5, n6, n8, n9, n10} from "./numbers";
+
+const lentele2 = `${daugiklis} * ${n1} = ${daugiklis * n1}
+${daugiklis} * ${n2} = ${daugiklis * n2}
+${daugiklis} * ${n3} = ${daugiklis * n3}
+${daugiklis} * ${n4} = ${daugiklis * n4}
+${daugiklis} * ${n5} = ${daugiklis * n5}
+${daugiklis} * ${n6} = ${daugiklis * n6}
+${daugiklis} * ${n7} = ${daugiklis * n7}
+${daugiklis} * ${n8} = ${daugiklis * n8}
+${daugiklis} * ${n9} = ${daugiklis * n9}
+${daugiklis} * ${n10} = ${daugiklis * n10}`;
