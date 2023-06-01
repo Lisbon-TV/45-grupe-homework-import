7e6bf94b819e327c06ac6cb95177a5eb35752c75
Lisbon <lisbon@dublin.com>
2023-06-01 10:28:07 +0300

    Numbers n1, n2, etc.

--- js/03-import.js
+++ js/03-import.js
@@ -2,3 +2,9 @@ console.clear();
 
 import daugiklis from '../extra/multiplier.js';
 console.log(daugiklis);
+
+import {n1, n2, n3, n4, n5, n6, n8, n9, n10} from '../extra/numbers.js';
+console.log("................");
+
+console.log(n1, n2, n3, n4, n5, n6, n8, n9, n10);
+
