116d9c12b26e14d61541835c435ee8ed5c1c4d8b
Lisbon <lisbon@dublin.com>
2023-06-01 11:30:13 +0300

    Included full code for calculators 1 and 2 for visibility

---
+++ js/02- calculator.js
@@ -0,0 +1,52 @@
+// Calculators root code samples:
+
+const daugiklis = 5;
+const n1 = 1;
+const n2 = 2;
+const n3 = 3;
+const n4 = 4;
+const n5 = 5;
+const n6 = 6;
+const n7 = 7;
+const n8 = 8;
+const n9 = 9;
+const n10 = 10;
+
+const lentele1 = daugiklis + ' * ' + n1 + ' = ' + (daugiklis * n1) + '\r\n' + daugiklis + ' * ' + n2 + ' = ' + (daugiklis * n2) + '\r\n' + daugiklis + ' * ' + n3 + ' = ' + (daugiklis * n3) + '\r\n' + daugiklis + ' * ' + n4 + ' = ' + (daugiklis * n4) + '\r\n' + daugiklis + ' * ' + n5 + ' = ' + (daugiklis * n5) + '\r\n' + daugiklis + ' * ' + n6 + ' = ' + (daugiklis * n6) + '\r\n' + daugiklis + ' * ' + n7 + ' = ' + (daugiklis * n7) + '\r\n' + daugiklis + ' * ' + n8 + ' = ' + (daugiklis * n8) + '\r\n' + daugiklis + ' * ' + n9 + ' = ' + (daugiklis * n9) + '\r\n' + daugiklis + ' * ' + n10 + ' = ' + (daugiklis * n10);
+
+console.log(lentele1);
+console.log();
+
+const lentele2 = `${daugiklis} * ${n1} = ${daugiklis * n1}
+${daugiklis} * ${n2} = ${daugiklis * n2}
+${daugiklis} * ${n3} = ${daugiklis * n3}
+${daugiklis} * ${n4} = ${daugiklis * n4}
+${daugiklis} * ${n5} = ${daugiklis * n5}
+${daugiklis} * ${n6} = ${daugiklis * n6}
+${daugiklis} * ${n7} = ${daugiklis * n7}
+${daugiklis} * ${n8} = ${daugiklis * n8}
+${daugiklis} * ${n9} = ${daugiklis * n9}
+${daugiklis} * ${n10} = ${daugiklis * n10}`;
+
+console.log(lentele2);
+
+console.clear();
+
+const multiplier = 3;
+let number = 0;
+
+const m1 = multiplier * n1;
+const m2 = multiplier * n2;
+
+const lentele3 = `
+${multiplier} * ${++number} = ${multiplier * number}
+${multiplier} * ${++number} = ${multiplier * number}
+${multiplier} * ${++number} = ${multiplier * number}
+${multiplier} * ${++number} = ${multiplier * number}
+${multiplier} * ${++number} = ${multiplier * number}
+${multiplier} * ${++number} = ${multiplier * number}
+${multiplier} * ${++number} = ${multiplier * number}
+${multiplier} * ${++number} = ${multiplier * number}
+${multiplier} * ${++number} = ${multiplier * number}
+${multiplier} * ${++number} = ${multiplier * number}
+`;
